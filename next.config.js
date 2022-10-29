3a8dde654686cee1439584782a2d894310547b12
Rafael Vidal <vidal@live.com>
2022-10-29 22:43:41 +0800

    add link to domains allowed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: ["yt3.ggpht.com"],
+	},
 }
 
 module.exports = nextConfig

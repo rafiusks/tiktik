5fb81f55650d4862b95296495e056b0c201ec57b
Rafael Vidal <vidal@live.com>
2022-10-29 23:44:59 +0800

    create or login user

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,9 +7,12 @@ import { GoogleLogin, googleLogout } from "@react-oauth/google"
 import { BiSearch } from "react-icons/bi"
 import { IoMdAdd } from "react-icons/io"
 import Logo from "../utils/tiktik-logo.png"
+import { createOrGetUser } from "../utils"
+import useAuthStore from "../store/authStore"
 
 const NavBar = () => {
-	const user = false
+	const { userProfile, addUser } = useAuthStore()
+
 	return (
 		<div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
 			<Link href='/'>
@@ -24,11 +27,11 @@ const NavBar = () => {
 			</Link>
 			<div>Search</div>
 			<div>
-				{user ? (
-					<div>Looged In</div>
+				{userProfile ? (
+					<div>Welcome back {userProfile.userName}</div>
 				) : (
 					<GoogleLogin
-						onSuccess={(response) => console.log(response)}
+						onSuccess={(response) => createOrGetUser(response, addUser)}
 						onError={() => console.log("Error")}
 					/>
 				)}

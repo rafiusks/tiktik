5e4652639257d8e7ae5d92c999a27e4d9273dfb4
Rafael Vidal <vidal@live.com>
2022-10-30 09:41:14 +0800

    create post in sanity backend

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -14,6 +14,10 @@ const upload = () => {
 		SanityAssetDocument | undefined
 	>()
 	const [wrongFileType, setWrongFileType] = useState(false)
+	const [caption, setCaption] = useState("")
+	const [category, setCategory] = useState(topics[0].name)
+	const [savingPost, setSavingPost] = useState(false)
+	const router = useRouter()
 
 	const uploadVideo = async (e: any) => {
 		const selectedFile = e.target.files[0]
@@ -32,6 +36,36 @@ const upload = () => {
 		} else {
 			setIsLoading(false)
 			setWrongFileType(true)
+		}
+	}
+
+	const { userProfile }: { userProfile: any } = useAuthStore()
+
+	const handlePost = async () => {
+		if (caption && videoAsset?._id && category) {
+			setSavingPost(true)
+
+			const document = {
+				_type: "post",
+				caption: caption,
+				video: {
+					_type: "file",
+					asset: {
+						_type: "reference",
+						_ref: videoAsset?._id,
+					},
+				},
+				userId: userProfile?._id,
+				postedBy: {
+					_type: "postedby",
+					_ref: userProfile?._id,
+				},
+				topic: category,
+			}
+
+			await axios.post("http://localhost:3000/api/post", document)
+
+			router.push("/")
 		}
 	}
 
@@ -100,13 +134,13 @@ const upload = () => {
 					<label className='text-md font-medium'>Caption</label>
 					<input
 						type='text'
-						value=''
-						onChange={() => {}}
+						value={caption}
+						onChange={(e) => setCaption(e.target.value)}
 						className='rounded outline-none text-md border-2 border-gray-200 p-2'
 					/>
 					<label className='text-md font-medium'>Choose a Category</label>
 					<select
-						onChange={() => {}}
+						onChange={(e) => setCategory(e.target.value)}
 						className='outline-none border-2 border-gray-200 text-md capitalize lg:p-4 p-2 rounded cursor-pointer'>
 						{topics.map((topic) => (
 							<option
@@ -125,7 +159,7 @@ const upload = () => {
 							Discard
 						</button>
 						<button
-							onClick={() => {}}
+							onClick={handlePost}
 							type='button'
 							className='bg-[#f51997] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none'>
 							Post
